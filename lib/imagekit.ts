e89b9a3e643bb313e05758b41f1c51f7da2a20c9
eng-rayash <rayash2025@gmail.com>
2026-06-24 22:53:08 +0300

    تعديل خطاء

--- lib/imagekit.ts
+++ lib/imagekit.ts
@@ -5,9 +5,7 @@ if (!process.env.IMAGEKIT_PRIVATE_KEY) {
 }
 
 export const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || '',
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY || '',
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || '',
 });
 
 export { toFile };
